a59457c3bb781081f2692747661e69c150e99cf7
Jonathan Marquez <jonathanmarquezj@gmail.com>
2020-05-26 12:59:45 +0200

    Archivo de credenciales

--- app.py
+++ app.py
@@ -342,6 +342,14 @@ def oauth2callback():
 
 	return flask.redirect(flask.url_for('test_api_request'))
 
+def credentials_to_dict(credentials):
+	return {'token': credentials.token,
+		'refresh_token': credentials.refresh_token,
+		'token_uri': credentials.token_uri,
+		'client_id': credentials.client_id,
+		'client_secret': credentials.client_secret,
+		'scopes': credentials.scopes}
+
 
 
 # Tienes que crear esta variable si no la tienes, en heroku no hace falta.
